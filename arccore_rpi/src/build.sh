16cb4f0f7e6f2b1a09f3d42dede0ce338b2d331e
Avenir Kobetski <avekob@gmail.com>
2014-12-23 08:47:05 +0100

    Modified build script

--- arccore_rpi/src/build.sh
+++ arccore_rpi/src/build.sh
@@ -2,21 +2,16 @@
 
 export BOARDDIR=Raspberry_Pi
 export BDIR=../examples/Raspberry_Pi/demo_VCU
+export BUILDDIR=.
 export CROSS_COMPILE=/opt/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/arm-none-eabi-
-export BUILDDIR=/home/avenir/Documents/dev/eclipse_ws/autosar/src/core
-#export BUILDDIR=/home/avenir/Documents/dev/fresta/autosar_v4/src/doip-1.4.3
-#export KVMDIR=/home/avenir/Documents/dev/fresta/j2me_cldc/build/autosar
 
 cd core
-make all
+if [ "$1" = "clean" ]; then
+	make clean
+else
+	make all
+fi
 
-#if [ "$1" == "clean" ]; then
-	#make clean
-	#	make clean_all #KVM=true 
-	#else
-#	make clean
-	#KVM=true 
-#fi
 
 
 
