b7f505f839cc6d7b646d2d0422d26974468285ff
Avenir Kobetski <avekob@gmail.com>
2014-12-23 08:36:16 +0100

    Modified build-script

--- arccore_rpi/src/examples/Raspberry_Pi/demo_VCU/obj_Raspberry_Pi/UltrasonicSensor.d
+++
@@ -1,9 +0,0 @@
-UltrasonicSensor.o: \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/arch/arm/arm_v6/drivers/sensors/UltrasonicSensor.c \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/arch/arm/arm_v6/drivers/bcm2835.h \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/include/Std_Types.h \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/include/Platform_Types.h \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/include/Compiler.h \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/arch/arm/arm_v6/drivers/bcm2835_Types.h \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/arch/arm/arm_v6/drivers/extern_defs.h \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/arch/arm/arm_v6/drivers/sensors/Sensors.h
--- arccore_rpi/src/examples/Raspberry_Pi/demo_VCU/obj_Raspberry_Pi/clib.d
+++
@@ -1,2 +0,0 @@
-clib.o: /home/avenir/Documents/dev/eclipse_ws/moped/src/core/clib/clib.c \
- /home/avenir/Documents/dev/eclipse_ws/moped/src/core/clib/clib.h
