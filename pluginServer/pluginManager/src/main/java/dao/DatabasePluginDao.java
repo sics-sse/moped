2b34b59c6ba73bb15b68fda571d2c6e184e901c0
Avenir Kobetski <avekob@gmail.com>
2014-12-29 09:02:05 +0100

    Rename pluginServer -> server & webportal

--- pluginServer/pluginManager/src/main/java/dao/DatabasePluginDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.DatabasePlugin;
-
-public interface DatabasePluginDao {
-	public DatabasePlugin getDatabasePlugin(int databasePluginId);
-	public void saveDatabasePlugin(DatabasePlugin databasePlugin);
-}
-
