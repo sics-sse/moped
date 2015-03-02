6e9a41587446635c4c10c82ec97e4b767dd9feba
Arndt Jonasson <arndt@sics.se>
2015-03-02 16:59:18 +0100

    Removed some unnecessary imports.

--- server/src/main/java/service/PluginWebServicesImpl.java
+++ server/src/main/java/service/PluginWebServicesImpl.java
@@ -30,14 +30,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.mina.core.session.IoSession;
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
