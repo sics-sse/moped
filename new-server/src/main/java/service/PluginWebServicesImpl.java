723b0bb5ba71580d4a0680fb238d42be04343f19
Arndt Jonasson <arndt@sics.se>
2015-03-05 11:15:29 +0100

    Removed unused import.

--- new-server/src/main/java/service/PluginWebServicesImpl.java
+++ new-server/src/main/java/service/PluginWebServicesImpl.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
