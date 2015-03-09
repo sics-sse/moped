c62cd73cb89ade8e42744ef282eff2f8384b9376
Arndt Jonasson <arndt@sics.se>
2015-03-09 09:49:21 +0100

    Removed unused files.

--- new-server/src/main/java/common/AllocationStrategy.java
+++
@@ -1,7 +0,0 @@
-package common;
-
-import java.io.Serializable;
-
-public enum AllocationStrategy implements Serializable {
-	RANDOM, SAME_ECU, DIFF_ECU, SPECIFIC, MAX_MEMORY, MAX_CPU, MAX_STORAGE
-}
