574a10f0e40e6473a1c4331dddb54d7e2749a9d9
Arndt Jonasson <arndt@sics.se>
2015-03-16 08:34:13 +0100

    Experimental, but maybe correct: don't use get_ack_status but check the installed apps instead.

--- new-server/misc/testmoped.py
+++ new-server/misc/testmoped.py
@@ -1,9 +1,19 @@
+import json
 
 def ack(client, app):
+    vin = "20UYA31581L000000"
     while True:
-        x = client.service.get_ack_status("20UYA31581L000000", app)
-        if x == True:
-            break
+        y = client.service.listInstalledApps()
+        y = json.loads(y)
+        for t in y['result']:
+            if t['vin'] == vin:
+                if int(t['appid']) == app:
+                    return
+
+#        x = client.service.get_ack_status(vin, app)
+#        print x
+#        if x == True:
+#            break
 
 def readfile(name):
     f = open(name)
