09299698d70ff85681bbd04328251a33c243da13
Arndt Jonasson <arndt@sics.se>
2015-03-17 16:49:23 +0100

    Changed the values to be the same as used in another instances of this file, in ecm-core.

--- api/src/main/java/sics/messages/MessageType.java
+++ api/src/main/java/sics/messages/MessageType.java
@@ -12,21 +12,23 @@ public class MessageType extends Enum {
 	 */
 	private static final long serialVersionUID = 1L;
 	public static final byte INIT = 0;
-	public static final byte INSTALL = 1;
-	public static final byte UNINSTALL = 2;
-	public static final byte INSTALL_ACK = 3;
-	public static final byte UNINSTALL_ACK = 4;
-	public static final byte RESTORE = 5;
-	public static final byte PLUGIN_MESSAGE = 6;
-	public static final byte PORT_LINK_CONTEXT_MESSAGE = 7;
-	public static final byte PORT_LINK_ACK_MESSAGE = 8;
-	public static final byte RESTORE_ACK = 9;
-	public static final byte LOAD = 10;
-	public static final byte LOAD_ACK = 11;
-	public static final byte PUBLISH = 12;
+	public static final byte INSTALL_ACK = 1;
+	public static final byte UNINSTALL_ACK = 2;
+	public static final byte PUBLISH = 3;
+	public static final byte INSTALL = 4;
+	public static final byte UNINSTALL = 5;
+	public static final byte RESTORE = 6;
+	public static final byte PLUGIN_MESSAGE = 7;
+	public static final byte PORT_LINK_CONTEXT_MESSAGE = 8;
+	public static final byte PORT_LINK_ACK_MESSAGE = 9;
+	public static final byte RESTORE_ACK = 10;
+	public static final byte LOAD = 11;
+	public static final byte LOAD_ACK = 12;
 	public static final byte SUBSCRIBE = 13;
 	public static final byte REQUEST_ID = 14;
 	public static final byte REQUEST_ID_ACK = 15;
+	public static final byte PWM = 16;
+	public static final byte INSTALL_LINUX_ACK = 17;
 	
 	protected MessageType(byte enumValue) {
 		super(enumValue);
