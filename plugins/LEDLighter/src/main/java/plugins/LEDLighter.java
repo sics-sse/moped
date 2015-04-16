4419395c194a308e3ef40215796e09b841c2b16f
Arndt Jonasson <arndt@sics.se>
2015-04-16 09:11:13 +0200

    Turn on and off all three LEDs, in order.

--- plugins/LEDLighter/src/main/java/plugins/LEDLighter.java
+++ plugins/LEDLighter/src/main/java/plugins/LEDLighter.java
@@ -9,18 +9,21 @@ import sics.PIRTE;
 public class LEDLighter extends PlugInComponent {
 	public PluginPPort led;
 	
+    private int k;
+
 	public LEDLighter(String[] args) {
 		super(args);
 	}
 	
 	public LEDLighter() {
+	    k = 0;
 	}
 	
 	public static void main(String[] args) {
-		VM.println("LEDLighter.main()\r\n");
+	    //		VM.println("LEDLighter.main()\r\n");
 		LEDLighter ledLighter = new LEDLighter(args);
 		ledLighter.run();
-		VM.println("LEDLighter-main done\r\n");
+		//		VM.println("LEDLighter-main done\r\n");
 	}
 
 //	public void setSpeed(PluginPPort speed) {
@@ -34,68 +37,51 @@ public class LEDLighter extends PlugInComponent {
 	public void init() {
 		// Initiate PluginPPort
 		led = new PluginPPort(this, "led");
+		VM.println("new pluginpport led");
 	}
 	
-	public void doFunction() {
-	    //VM.println("doFunction");
-		try {
-			for (int i = 0; i < 100; i++) {
-//				// Initial LEDs
-//				led.write("1|0");
-//				Thread.sleep(100);
-//				led.write("2|0");
-//				Thread.sleep(100);
-//				led.write("3|0");
-//				Thread.sleep(3000);
-//				// Light RED on
-//				led.write("1|1");
-//				Thread.sleep(3000);
-//				// Light RED off
-//				led.write("1|0");
-//				Thread.sleep(3000);
-//				// Light Yellow1 on
-//				led.write("2|1");
-//				Thread.sleep(3000);
-//				// Light Yellow1 off
-//				led.write("2|0");
-//				Thread.sleep(3000);
-//				// Light Yellow2 on
-//				led.write("3|1");
-//				Thread.sleep(3000);
-//				// Light Yellow2 off
-//				led.write("3|0");
-//				Thread.sleep(3000);
-//				// Light all leds on
-//				led.write("1|1");
-//				Thread.sleep(100);
-//				led.write("2|1");
-//				Thread.sleep(100);
-//				led.write("3|1");
-//				Thread.sleep(3000);
-//				// Light all leds off
-//				led.write("1|0");
-//				Thread.sleep(100);
-//				led.write("2|0");
-//				Thread.sleep(100);
-//				led.write("3|0");
-//				Thread.sleep(20000);
-				led.write("1|0");
-				Thread.sleep(2000);
-				led.write("1|1");
-				Thread.sleep(10000);
-//				led.write("2|0");
-//				Thread.sleep(2000);
-//				led.write("2|1");
-//				Thread.sleep(10000);
-//				led.write("3|0");
-//				Thread.sleep(2000);
-//				led.write("3|1");
-//				Thread.sleep(10000);
-			}
-		} catch (InterruptedException e) {
-		    //VM.println("Interrupted.\r\n");
-		}
+    //    public void iter(int p, int e) {
+    //	switch(p) {
+    //	case 1:
+    //	    VM.println("a");
+    //	    break;
+    //	case 2:
+    //	    VM.println("b");
+    //	    break;
+    //	default:
+    //	    VM.println("c");
+    //	}
+    //    }
+
+    private void sleep(int ms) {
+    	try {
+    	    Thread.sleep(ms);
+    	} catch (InterruptedException e) {
+    	    VM.println("Interrupted.\r\n");
+    	}
+    }
+
+    public void doFunction2() {
+		VM.println("2|1");
+		led.write("2|1");
+    }
+
+    private void setled(String str) {
+	led.write(str);
+	//	VM.println(str);
+	sleep(500);
+    }
+
+    public void doFunction() {
+	while (true) {
+	    setled("1|0");
+	    setled("2|0");
+	    setled("3|0");
+	    setled("1|1");
+	    setled("2|1");
+	    setled("3|1");
 	}
+    }
 
 	public void run() {
 	    init();
