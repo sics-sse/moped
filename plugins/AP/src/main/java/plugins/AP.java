093f2efd42e03f144c6c50911d03e70c48df85e2
Arndt Jonasson <arndt@sics.se>
2015-04-16 09:09:12 +0200

    Read wheel speed, and reverse if the wheel speed is too low when we ought to be moving forward. Not perfect, but the car does get itself out from obstacles.

--- plugins/AP/src/main/java/plugins/AP.java
+++ plugins/AP/src/main/java/plugins/AP.java
@@ -4,12 +4,16 @@ import com.sun.squawk.VM;
 import java.lang.Math;
 import sics.plugin.PlugInComponent;
 import sics.port.PluginPPort;
+import sics.port.PluginRPort;
 import sics.PIRTE;
 
 public class AP extends PlugInComponent {
 	public PluginPPort speed;
 	public PluginPPort steering;
 	
+    public PluginRPort rspeed;
+    public PluginRPort rsteer;
+
 	public AP(String[] args) {
 		super(args);
 	}
@@ -34,14 +38,37 @@ public class AP extends PlugInComponent {
 	
 	public void init() {
 		// Initiate PluginPPort
+	    VM.println("init 1");
 		speed = new PluginPPort(this, "sp");
+	    VM.println("init 2");
 		steering = new PluginPPort(this, "st");
+	    VM.println("init 3");
+		rspeed = new PluginRPort(this, "rsp");
+	    VM.println("init 4");
+		rsteer = new PluginRPort(this, "rst");
+	    VM.println("init 5");
 	}
 	
+    private void show(String msg, PluginRPort port) {
+	//VM.println("show 1");
+	//Object obj = port.receive();
+	//String obj = port.readString();
+	int x = port.readInt();
+	String obj = "(" + x + ")";
+	//VM.println("show 2");
+	if (obj == null) {
+	    VM.println("received from port " + msg + ": " + "null");
+	} else {
+	    VM.println("received from port " + msg + ": " + (String) obj);
+	}
+    }
+
+    // TODO: check getting stuck moving backwards too
+
 	public void doFunction() {
+	    int s;
 		try {
 			for (int i = 0; i < 100; i++) {
-			    //VM.println("[AP is running]");
 				
 				// warmup
 				Thread.sleep(2000);
@@ -49,24 +76,114 @@ public class AP extends PlugInComponent {
 				steering.write(0);
 				Thread.sleep(2000);
 				// forward
+
+				show("speed", rspeed);
+				//show("steering", rsteer);
+
 				speed.write(20);
 				steering.write(0);
 				Thread.sleep(2000);
+
+				s = rspeed.readInt();
+				VM.println("s = " + s);
+				if (s < 5) {
+				    VM.println("reversing");
+				    speed.write(0);
+				    steering.write(0);
+				    Thread.sleep(3000);
+				    speed.write(-10);
+				    steering.write(0);
+				    show("speed", rspeed);
+				    Thread.sleep(7000);
+				    show("speed", rspeed);
+				}
 
 				// turn left
 				speed.write(20);
 				steering.write(-80);
 				Thread.sleep(3000);
 
+				s = rspeed.readInt();
+				VM.println("s = " + s);
+				//s = 1;
+				if (s < 5) {
+				    VM.println("reversing");
+				    speed.write(0);
+				    steering.write(0);
+				    Thread.sleep(3000);
+				    speed.write(-30);
+				    steering.write(0);
+				    show("speed", rspeed);
+				    Thread.sleep(7000);
+				    show("speed", rspeed);
+				}
+
+				show("speed", rspeed);
+				//show("steering", rsteer);
+
 				// right, right
 				speed.write(20);
 				steering.write(80);
-				Thread.sleep(6000);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+				Thread.sleep(500);
+				show("speed", rspeed);
+
+				//show("steering", rsteer);
+
+				s = rspeed.readInt();
+				VM.println("s = " + s);
+				if (s < 5) {
+				    VM.println("reversing");
+				    speed.write(0);
+				    steering.write(0);
+				    Thread.sleep(3000);
+				    speed.write(-10);
+				    steering.write(0);
+				    show("speed", rspeed);
+				    Thread.sleep(7000);
+				    show("speed", rspeed);
+				}
 
 				// straight
 				speed.write(20);
 				steering.write(0);
 				Thread.sleep(4000);
+
+				s = rspeed.readInt();
+				VM.println("s = " + s);
+				if (s < 5) {
+				    VM.println("reversing");
+				    speed.write(0);
+				    steering.write(0);
+				    Thread.sleep(3000);
+				    speed.write(-10);
+				    steering.write(0);
+				    show("speed", rspeed);
+				    Thread.sleep(7000);
+				    show("speed", rspeed);
+				}
 
 				// stop
 				speed.write(0);
