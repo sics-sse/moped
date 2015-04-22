8a20c7eb3fcd2e5e4472222c79f237a4be54e331
Arndt Jonasson <arndt@sics.se>
2015-04-22 09:00:59 +0200

    "set -e" and a comment.

--- new-server/misc/build1.sh
+++ new-server/misc/build1.sh
@@ -1,5 +1,12 @@
 #/bin/bash
 
+set -e
+
+# Ensure that JAVA_HOME is set correctly,
+# and LD_LIBRARY_PATH according to the output from "d.sh jvmenv".
+# Also, ensure that PATH is set so the cross compiler is found
+# (Sourcery_CodeBench_Lite_for_ARM_EABI or something similar).
+
 ROOT=~/moped/moped
 
 HERE=$ROOT/new-server/misc
@@ -15,4 +22,7 @@ cd $AUTOSAR
 ./build.sh
 
 cd $HERE
-./build2.sh LEDLighter 1.0
+#./build2.sh AutoBrake 1.0
+#./build2.sh UltraSonicReader 1.0
+./build2.sh Comm1 1.0
+./build2.sh Comm2 1.0
