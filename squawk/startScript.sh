d1d8d0457787682dcb7087f501ed6de9351b04f6
Arndt Jonasson <arndt@sics.se>
2015-04-22 10:37:59 +0200

    Added "set -e"

--- squawk/startScript.sh
+++ squawk/startScript.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 ./d.sh clean
 
 cd builder
@@ -6,7 +9,7 @@ cd builder
 cd ..
 
 ./d.sh -comp:gcc-rpi-autosar copyphoneme
-./d.sh -comp:gcc-rpi-autosar
+./d.sh -comp:gcc-rpi-autosar # -verbose
 ./d.sh -comp:gcc-rpi-autosar -prod -mac -o2 -verbose rom -metadata cldc #imp debugger
 ./createSquawkArray.sh
 ./d.sh -comp:gcc-rpi-autosar
