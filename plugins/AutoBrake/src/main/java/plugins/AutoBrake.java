8d873dfd9c8005ad5aa9a8be02a8cdfde7732f5e
Arndt Jonasson <arndt@sics.se>
2015-04-27 10:38:00 +0200

    Added a thread mechanism so we don't hang when we read from a port that no one is writing to. This ought to be done in PIRTE, since we can't kill these threads.

--- plugins/AutoBrake/src/main/java/plugins/AutoBrake.java
+++ plugins/AutoBrake/src/main/java/plugins/AutoBrake.java
@@ -1,13 +1,37 @@
-package tests;
+package plugins;
 
 import java.io.IOException;
 import com.sun.squawk.VM;
-import com.sun.squawk.io.mailboxes.Channel;
-import com.sun.squawk.io.mailboxes.Envelope;
-import com.sun.squawk.io.mailboxes.ByteArrayEnvelope;
 import sics.port.PluginPPort;
 import sics.port.PluginRPort;
 import sics.plugin.PlugInComponent;
+
+class WorkThread extends Thread {
+
+    PluginRPort port;
+    Object obj;
+
+    WorkThread(PluginRPort port) {
+	this.port = port;
+	this.obj = null;
+    }
+
+    public void run() {
+	Object obj = port.receive();
+
+	if (false) {
+	    try {
+		Thread.sleep(4);
+	    } catch (InterruptedException e) {
+		//VM.println("Interrupted.\r\n");
+	    }
+	}
+	//obj = (Object) "hej";
+
+	this.obj = obj;
+	//VM.println("WorkThread done");
+    }
+}
 
 public class AutoBrake extends PlugInComponent {
 	private PluginRPort ab;
@@ -36,37 +60,67 @@ public class AutoBrake extends PlugInComponent {
 	
 	public void run() {}
 
+    private Object getval(PluginRPort port) {
+	WorkThread p = new WorkThread(ab);
+	p.start();
+	try {
+	    Thread.sleep(3000);
+	} catch (InterruptedException e) {
+	    VM.println("Interrupted.\r\n");
+	}
+	Object o2 = p.obj;
+	VM.println("plupp " + o2);
+	if (p.obj != null) {
+	    //p.stop();
+	}
+	return o2;
+    }
+
 	public void doFunction() {
-		String data;
+	    String data;
 		
-		VM.println("[AutoBrake is running]");
-		while (true) {
-			Object obj = ab.receive();
-			if(obj != null) {
-				String distanceStr = (String)obj;
-				VM.print("distance:");
-				VM.println(distanceStr);
-				
-				int distance = Integer.parseInt(distanceStr);
-				if (distance < 30) {
-					VM.jnaSetSelect(1);
-					brake.write(-100);
-					
-					brakeLight.write("1|0"); // "pin_nr (red/yellow_1/yellow_2) | on/off (0/1)" // Turn on red light
-				}
-				else {
-					brake.write(0);
-					VM.jnaSetSelect(0);
-					
-					brakeLight.write("1|1"); // "pin_nr (red/yellow_1/yellow_2) | on/off (0/1)" // Turn off red light
-				}
-			}
-			
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				VM.println("Interrupted.\r\n");
-			}
+	    VM.println("[AutoBrake is running] 1");
+
+
+
+	    VM.println("[AutoBrake is running] 2");
+	    while (true) {
+		try {
+		    Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		    VM.println("Interrupted.\r\n");
 		}
+
+		try {
+		    //Object obj = ab.receive();
+		    Object obj = getval(ab);
+		    if(obj != null) {
+			VM.println("ab returned an object");
+			VM.println("ab returned an object: " + obj);
+			String distanceStr = (String)obj;
+			VM.print("distance:");
+			VM.println(distanceStr);
+				
+			int distance = Integer.parseInt(distanceStr);
+			if (distance < 30) {
+			    VM.jnaSetSelect(1);
+			    brake.write(-100);
+					
+			    brakeLight.write("1|0"); // "pin_nr (red/yellow_1/yellow_2) | on/off (0/1)" // Turn on red light
+			}
+			else {
+			    brake.write(0);
+			    VM.jnaSetSelect(0);
+					
+			    brakeLight.write("1|1"); // "pin_nr (red/yellow_1/yellow_2) | on/off (0/1)" // Turn off red light
+			}
+		    } else {
+			VM.println("ab returned null");
+		    }
+			
+		} catch (Exception e) {
+		    VM.println("AutoBrake: exception");
+		}
+	    }
 	}
 }
