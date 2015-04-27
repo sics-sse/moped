adb925514e50d5d034f6875c1b3ebae80cc6b902
Arndt Jonasson <arndt@sics.se>
2015-04-27 10:34:53 +0200

    Added constructors.

--- plugins/PluginCreationTest3/src/main/java/plugins/PluginCreationTest3.java
+++ plugins/PluginCreationTest3/src/main/java/plugins/PluginCreationTest3.java
@@ -1,5 +1,6 @@
 package plugins;
 
+import com.sun.squawk.VM;
 import sics.plugin.PlugInComponent;
 import sics.port.PluginPPort;
 
@@ -7,11 +8,21 @@ public class PluginCreationTest3 extends PlugInComponent {
 
 	public PluginPPort steering;
 
+	public PluginCreationTest3(String[] args) {
+		super(args);
+		VM.println("ran constructor(args)");
+	}
+	
+	public PluginCreationTest3() {
+		super();
+		VM.println("ran constructor()");
+	}
+	
 	@Override
 	public void run() {
-		System.out.println("PluginCreationTest3 running");
+	    VM.println("PluginCreationTest3 running");
 
-		HelpClass c = new HelpClass();
+	    //	    HelpClass c = new HelpClass();
 
 		init();
 
@@ -19,19 +30,25 @@ public class PluginCreationTest3 extends PlugInComponent {
 
 	@Override
 	public void init() {
-		System.out.println("PluginCreationTest3 initializing 3...");
+	    VM.println("PluginCreationTest3 initializing 3...");
 
 		steering = new PluginPPort(this, "steeringAngle");
 
+	    VM.println("steering");
+
 		steering.write(-50);
 
-		System.out.println("and done!");
+		VM.println("and done!");
 	}
 
 	public static void main(String[] args) {
-		System.out.println("In PluginCreationTest3.main()");
-		PluginCreationTest3 instance = new PluginCreationTest3();
+	    VM.println("In PluginCreationTest3.main()");
+	    for (int i = 0; i < args.length; i++) {
+		VM.println("arg " + args[i]);
+	    }
+		PluginCreationTest3 instance = new PluginCreationTest3(args);
 
+	    VM.println("instance created");
 		instance.run();
 	}
 }
