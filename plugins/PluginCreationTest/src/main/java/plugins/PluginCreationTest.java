ebdb0dd972c532fccdce0adbf7b233021f7bc44d
Arndt Jonasson <arndt@sics.se>
2015-04-27 10:33:53 +0200

    Print using VM.

--- plugins/PluginCreationTest/src/main/java/plugins/PluginCreationTest.java
+++ plugins/PluginCreationTest/src/main/java/plugins/PluginCreationTest.java
@@ -1,26 +1,31 @@
 package plugins;
 
+import com.sun.squawk.VM;
+
 import sics.plugin.PlugInComponent;
 
 public class PluginCreationTest extends PlugInComponent {
 
 	@Override
 	public void run() {
-		System.out.println("PluginCreationTest running");
+	    VM.println("hej1");
+	    //VM.println("PluginCreationTest running");
 		init();
 
 	}
 
 	@Override
 	public void init() {
-		System.out.println("PluginCreationTest initializing...");
-		System.out.println("and done!");
-		System.out.println("Howdy!");
+	    VM.println("hej2");
+	    //VM.println("PluginCreationTest initializing...");
+	    //VM.println("and done!");
+	    //VM.println("Howdy!");
 	}
 
 	public static void main(String[] args) {
-		System.out.println("In PluginCreationTest.main()");
-		PluginCreationTest instance = new PluginCreationTest();
-		instance.run();
+	    //VM.println("In PluginCreationTest.main()");
+	    VM.println("hej3");
+	    //		PluginCreationTest instance = new PluginCreationTest();
+	    //		instance.run();
 	}
 }
