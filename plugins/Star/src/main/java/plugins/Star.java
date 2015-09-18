52f141b7d68c3d8ef8a33de2158687d9e5cb4ef9
Arndt Jonasson <arndt@sics.se>
2015-09-18 11:30:01 +0200

    The car goes back and forth, turning left and right, so it remains almost in place, only changing direction.

---
+++ plugins/Star/src/main/java/plugins/Star.java
@@ -0,0 +1,95 @@
+package plugins;
+
+import com.sun.squawk.VM;
+import java.lang.Math;
+import sics.plugin.PlugInComponent;
+import sics.port.PluginPPort;
+import sics.port.PluginRPort;
+
+public class Star extends PlugInComponent {
+    public PluginPPort speed;
+    public PluginPPort steering;
+    public PluginRPort fw;
+	
+    public Star(String[] args) {
+	super(args);
+    }
+	
+    public Star() {
+    }
+	
+    public static void main(String[] args) {
+	VM.println("Star.main()\r\n");
+	Star ap = new Star(args);
+	ap.run();
+	VM.println("Star-main done");
+    }
+
+    public void init() {
+	// Initiate PluginPPort
+	speed = new PluginPPort(this, "sp");
+	steering = new PluginPPort(this, "st");
+	fw = new PluginRPort(this, "fw");
+    }
+	
+    public void doFunction() {
+	int st1 = 70;
+
+	try {
+	    VM.println("star 1");
+	    Thread.sleep(2000);
+	    speed.write(0);
+	    steering.write(st1);
+
+	    VM.println("star 2");
+	    Thread.sleep(2000);
+	    speed.write(0);
+	    steering.write(st1);
+	    Thread.sleep(2000);
+	} catch (InterruptedException e) {
+	    //VM.println("Interrupted.");
+	}
+
+	int count = 0;
+	int dir = 1;
+
+	while (true) {
+	    count++;
+
+	    VM.println("star3 " + count + " " + dir);
+
+	    try {
+		steering.write(dir*st1);
+		if (dir > 0)
+		    speed.write(15);
+		else
+		    speed.write(-20);
+		Thread.sleep(1000);
+	    } catch (InterruptedException e) {
+		//VM.println("Interrupted.");
+	    }
+
+	    int frontWheelSpeedData = fw.readInt();
+
+	    VM.println("star4 " + count + " " + dir*frontWheelSpeedData);
+
+	    try {
+		speed.write(0);
+		steering.write(0);
+		// 500 is sometimes too little for the motor to be able
+		// to switch to the other direction in the next step
+		// 1000 is enough, and 600 seems to be, too
+		Thread.sleep(600);
+	    } catch (InterruptedException e) {
+		//VM.println("Interrupted.");
+	    }
+
+	    dir = -dir;
+	}
+    }
+
+    public void run() {
+	init();
+	doFunction();
+    }
+}
