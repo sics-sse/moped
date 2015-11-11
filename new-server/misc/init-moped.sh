92e55c20c383abb47e62641c66123c939938d097
Arndt Jonasson <arndt@sics.se>
2015-11-11 14:09:20 +0100

    Updated.

--- new-server/misc/init-moped.sh
+++ new-server/misc/init-moped.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
 
-# We should be inside moped/new-server/misc.
-# You need the python module suds somewhere; modify PYTHONPATH below
+# Current directory should be moped/new-server/misc.
+# You need the python module "suds" somewhere; modify PYTHONPATH below
 # accordingly.
-# There should be a newly started server and simulator running.
+# There should be a newly started server and car or simulator running.
+# If there is no car or simulator, only the last call will fail.
 
-# For expected answers, see init-moped.txt
+# Modify the SQL definition below if needed.
+# Modify USERDUMP so it refers to a mysql dump of wordpress's user table
+#  wp_users, modified so it enters the database "fresta2".
+# Set USERID to your id in the SQL user database.
 
 SQL="mysql -uroot -proot"
+
+USERDUMP=~/moped/wp_users.dump
+
+USERID=33
 
 if true; then
     $SQL <<EOF
@@ -22,7 +30,7 @@ EOF
 
     $SQL < ../fresta2-schema
 
-    (echo "use fresta2";cat ~/moped/wp_users.dump) | mysql -uroot -proot
+    (echo "use fresta2";cat $USERDUMP) | $SQL
 
 fi
 
@@ -35,52 +43,29 @@ client.options.cache.clear()
 s = client.service
 
 from testmoped import *
+import testmoped
 import base64
 
-n="PluginCreationTest3"
-v="1.0"
-print upload(s,n,v)
-
-x=s.compileApp(n, v)
+x=base64.b64encode(readfile("../../simulator/configs/system1.xml"))
+x = s.addVehicleConfig("MOPED", x)
 print x
 
-x=base64.b64encode(readfile("/home/arndt/moped/moped/simulator/configs/system1.xml"))
-x = client.service.addVehicleConfig("MOPED", x)
+x = s.addVehicle("minMOPED", "20UYA31581L000000", "MOPED")
 print x
 
-x = client.service.addVehicle("minMOPED", "20UYA31581L000000", "MOPED")
+x = s.addUserVehicleAssociation($USERID, "20UYA31581L000000", True)
 print x
 
-x=client.service.addUserVehicleAssociation(33, "20UYA31581L000000", True)
+app = testmoped.uploadplus(s, "DistPub", "1.0")
+print app
+
+x = s.installApp("20UYA31581L000000", app)
 print x
 
-x = client.service.installApp("20UYA31581L000000", 200)
-print x
-
-ack(client, 200)
-
-n="PluginCreationTest2"
-v="1.8"
-print upload(s,n,v)
-
-x=s.compileApp(n, v)
-print x
-
-x = client.service.installApp("20UYA31581L000000", 201)
-print x
-
-ack(client, 201)
-
-
-#x = client.service.uninstallApp("20UYA31581L000000", 200)
-#print x
-
-#x = client.service.uninstallApp("20UYA31581L000000", 201)
-#print x
 
 EOF
 
 for t in Vehicle VehiclePlugin VehicleConfig Ecu Port Link PluginPortConfig PluginLinkConfig AppConfig; do
     echo "*** $t *******"
-    echo "use fresta2; select * from $t;" | mysql -uroot -proot
+    echo "use fresta2; select * from $t;" | $SQL
 done
