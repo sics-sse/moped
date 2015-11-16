1b373d5c39e871218acdac505990fbbc116e6c86
Arndt Jonasson <arndt@sics.se>
2015-11-16 15:08:43 +0100

    Disabled the old "id" and "platoon" code. Now simply send it on as a pluginMessage.

--- ecm-core/src/main/java/network/external/IoTIMessageHandler.java
+++ ecm-core/src/main/java/network/external/IoTIMessageHandler.java
@@ -13,24 +13,29 @@ public class IoTIMessageHandler implements IMessage {
 
 	@Override
 	public void receive(String msg) {
-		System.out.println("[IOT] - " + msg);
+	    System.out.println("[IOT] - " + msg);
+	    PluginMessage pluginMessage;
+	    pluginMessage = new PluginMessage(-1, msg);
+	    ecm.process(pluginMessage);
+	    if (false) {
 		int startIdIndex = msg.indexOf("\"id\":") + 6;
 		String stripPrefix =  msg.substring(startIdIndex);
 		int endIdIndex =stripPrefix.indexOf("\",\"");
 		String key = stripPrefix.substring(0, endIdIndex);
 		if(key.equals("platoon")) {
-			int startValIndex = stripPrefix.indexOf("\"current_value\":");
-			stripPrefix = stripPrefix.substring(startValIndex + 17);
-			int endValIndex = stripPrefix.indexOf("\"}]}");
-			String val = stripPrefix.substring(0, endValIndex);
+		    int startValIndex = stripPrefix.indexOf("\"current_value\":");
+		    stripPrefix = stripPrefix.substring(startValIndex + 17);
+		    int endValIndex = stripPrefix.indexOf("\"}]}");
+		    String val = stripPrefix.substring(0, endValIndex);
 			
-			// TODO: refine package encapsulation
-			// forward to specific ECU
-			PluginMessage pluginMessage = new PluginMessage(-1, val);
-			ecm.process(pluginMessage);
+		    // TODO: refine package encapsulation
+		    // forward to specific ECU
+		    pluginMessage = new PluginMessage(-1, val);
+		    ecm.process(pluginMessage);
 		} else {
-			// ignore other values so far
+		    // ignore other values so far
 		}
+	    }
 	}
 
 }
