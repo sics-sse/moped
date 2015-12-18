345c276873a2638a103b99d11d7701ac4b32e7b9
Arndt Jonasson <arndt@sics.se>
2015-12-18 14:38:47 +0100

    Added sendToVCU.

--- ecm-core/src/main/java/network/internal/EcuManager.java
+++ ecm-core/src/main/java/network/internal/EcuManager.java
@@ -7,5 +7,6 @@ public interface EcuManager extends Runnable {
 	public void sendMessage(Message message);
 	public void setEcm(Ecm ecm);
 	public Ecm getEcm();
+    public void sendToVCU(String str, int portid);
 //	public void loadPlugins(int ecuId);
 }
