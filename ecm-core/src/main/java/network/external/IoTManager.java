c6addb9d232718a57b0b1bad6cccca4696a2e4c3
Arndt Jonasson <arndt@sics.se>
2015-12-18 14:46:32 +0100

    Added methods and attributes for subscribing.

--- ecm-core/src/main/java/network/external/IoTManager.java
+++ ecm-core/src/main/java/network/external/IoTManager.java
@@ -1,23 +1,32 @@
 package network.external;
 
-//import ecm.Ecm;
+import ecm.Ecm;
 import io.IPublisher;
 import io.IReceiver;
 import messages.PublishPacket;
 
 public class IoTManager implements Runnable {
-//	private Ecm ecm;
+	private Ecm ecm;
 	private IPublisher publisher;
-//	private IReceiver receiver;
+	private IReceiver receiver;
 	
 	public IoTManager(IPublisher publisher) { //, IReceiver receiver) {
 		this.publisher = publisher;
-//		this.receiver = receiver;
+		this.receiver = null;
+		this.ecm = null;
+	}
+	
+    public IoTManager(IPublisher publisher, IReceiver receiver) {
+		this.publisher = publisher;
+		this.receiver = receiver;
 	}
 	
 	public void run() {
-//		IoTIMessageHandler ioTIMessageHandler = new IoTIMessageHandler(ecm);
-//		receiver.subscribe(ioTIMessageHandler);
+	    if (receiver != null) {
+		IoTIMessageHandler ioTIMessageHandler = new IoTIMessageHandler(ecm);
+		System.out.println("subscribe IoTIMessageHandler");
+		receiver.subscribe(ioTIMessageHandler);
+	    }
 	}
 
 	public void sendPacket(PublishPacket packet) {
@@ -26,7 +35,7 @@ public class IoTManager implements Runnable {
 		publisher.publish(key, value);
 	}
 	
-//	public void setEcm(Ecm ecm) {
-//		this.ecm = ecm;
-//	}
+	public void setEcm(Ecm ecm) {
+		this.ecm = ecm;
+	}
 }
