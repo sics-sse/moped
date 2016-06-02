b099eccf56501e670cf787553417b26d90751628
Arndt Jonasson <arndt@sics.se>
2016-06-02 15:00:08 +0200

    Removed unused function.

--- api/src/main/java/sics/port/PluginRPort.java
+++ api/src/main/java/sics/port/PluginRPort.java
@@ -48,9 +48,4 @@ public class PluginRPort implements PluginPort {
 	public Object receive() {
 		return null;
 	}
-
-	private int byteArrayToInt(byte[] b) {
-		return b[3] & 0xFF | (b[2] & 0xFF) << 8 | (b[1] & 0xFF) << 16
-				| (b[0] & 0xFF) << 24;
-	}
 }
