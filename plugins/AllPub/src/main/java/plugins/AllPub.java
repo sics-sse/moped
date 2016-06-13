da2186a5ed0b626e7455deb7da7215ac53827821
Arndt Jonasson <arndt@sics.se>
2016-06-13 15:30:51 +0200

    Handle InterruptedException correctly.

--- plugins/AllPub/src/main/java/plugins/AllPub.java
+++ plugins/AllPub/src/main/java/plugins/AllPub.java
@@ -51,7 +51,7 @@ class WorkThread extends Thread {
 		VM.println("AllPub.main() 2");
 		publish.init();
 		VM.println("AllPub.main() 3");
-		publish.doFunction();
+		publish.run();
 		VM.println("AllPub-main done");
 	}
 
@@ -66,16 +66,19 @@ class WorkThread extends Thread {
 		led = new PluginPPort(this, "led");
 	}
 	
-	public void run() {}
+	public void run() {
+	    try {
+		doFunction();
+	    } catch (InterruptedException e) {
+		VM.println("**************** Interrupted.");
+		return;
+	    }
+	}
 
-    private Object getval(PluginRPort port) {
+    private Object getval(PluginRPort port) throws InterruptedException {
 	WorkThread p = new WorkThread(ab);
 	p.start();
-	try {
-	    Thread.sleep(1000);
-	} catch (InterruptedException e) {
-	    VM.println("Interrupted.");
-	}
+	Thread.sleep(1000);
 	Object o2 = p.obj;
 	//VM.println("plupp " + o2);
 	if (p.obj != null) {
@@ -84,192 +87,164 @@ class WorkThread extends Thread {
 	return o2;
     }
 
-	public void doFunction() {
-	    Boolean pub = true;
-	    int w = 500;
-		String data;
-		int val = 0;
-		int counter = 0;
-		VM.println("[AllPub is running]");
-		int mcnt = 0;
-		String pstr = "";
-		int delay1 = 200;
-		int delay2 = 200;
+    public void doFunction() throws InterruptedException {
+	Boolean pub = true;
+	int w = 500;
+	String data;
+	int val = 0;
+	int counter = 0;
+	VM.println("[AllPub is running]");
+	int mcnt = 0;
+	String pstr = "";
+	int delay1 = 200;
+	int delay2 = 200;
 
-		while (true) {
-		    mcnt += 1;
+	while (true) {
+	    mcnt += 1;
 
-		    int rearWheelSpeedData = rbw.readInt();
+	    int rearWheelSpeedData = rbw.readInt();
 
-		    if (false) {
-			rearWheelSpeedData = val;
-			val += 2;
+	    if (false) {
+		rearWheelSpeedData = val;
+		val += 2;
 
-			if (val >= 1000)
-			    val -= 1000;
-		    }
+		if (val >= 1000)
+		    val -= 1000;
+	    }
 
-		    counter++;
+	    counter++;
 
-		    if (false) {
-			if (pub)
-			    pbw.write("cnt|" + counter);
+	    if (false) {
+		if (pub)
+		    pbw.write("cnt|" + counter);
 
-			try {
-			    Thread.sleep(w);
-			} catch (InterruptedException e) {
-			    VM.println("Interrupted.\r\n");
-			}
-		    }
+		Thread.sleep(w);
+	    }
 
-		    if (false) {
-			VM.println("bw1 (" + counter + ") = " + rearWheelSpeedData);
-			data = "bw|" + String.valueOf(rearWheelSpeedData);
-			if (pub)
-			    pbw.write(data);
+	    if (false) {
+		VM.println("bw1 (" + counter + ") = " + rearWheelSpeedData);
+		data = "bw|" + String.valueOf(rearWheelSpeedData);
+		if (pub)
+		    pbw.write(data);
 
-			try {
-			    Thread.sleep(w);
-			} catch (InterruptedException e) {
-			    VM.println("Interrupted.\r\n");
-			}
-		    }
+		Thread.sleep(w);
+	    }
 
-		    int frontWheelSpeedData = fw.readInt();
-		    if (false) {
+	    int frontWheelSpeedData = fw.readInt();
+	    if (false) {
 
-			VM.println("speed = " + frontWheelSpeedData);
-			data = "speed|" + String.valueOf(frontWheelSpeedData);
-			if (pub)
-			    pbw.write(data);
+		VM.println("speed = " + frontWheelSpeedData);
+		data = "speed|" + String.valueOf(frontWheelSpeedData);
+		if (pub)
+		    pbw.write(data);
 
-			try {
-			    Thread.sleep(w);
-			} catch (InterruptedException e) {
-			    VM.println("Interrupted.\r\n");
-			}
-		    }
+		Thread.sleep(w);
+	    }
 
-		    String adcstring = "-1";
-		    adcstring = adc.readString();
-		    if (false) {
-			adcstring = adc.readString();
+	    String adcstring = "-1";
+	    adcstring = adc.readString();
+	    if (false) {
+		adcstring = adc.readString();
 
-			if (adcstring == null)
-			    adcstring = "null";
-			VM.println("adc = " + adcstring);
-			if (false) {
-			    data = "adc|" + adcstring;
-			    if (pub)
-				pbw.write(data);
+		if (adcstring == null)
+		    adcstring = "null";
+		VM.println("adc = " + adcstring);
+		if (false) {
+		    data = "adc|" + adcstring;
+		    if (pub)
+			pbw.write(data);
 
-			    try {
-				Thread.sleep(w);
-			    } catch (InterruptedException e) {
-				VM.println("Interrupted.\r\n");
-			    }
-			}
-		    }
-
-		    int x;
-		    if (true) {
-			// does it still hang if we don't receive what
-			// SCU sends?
-			// answer: yes
-
-			//Object obj = getval(ab);
-			Object obj = ab.receive();
-			if (obj != null) {
-			    String s = (String) obj;
-			    try {
-				x = Integer.parseInt(s);
-			    } catch (NumberFormatException e) {
-				VM.println("format exception (" + s + ")");
-				x = -1;
-			    }
-			} else {
-			    x = -1;
-			}
-			VM.println("dist = " + x);
-
-			if (false) {
-			    // does it still hang if we don't publish
-			    // what we received?
-			    // answer: yes
-			    try {
-				Thread.sleep(w);
-			    } catch (InterruptedException e) {
-				VM.println("Interrupted.\r\n");
-			    }
-
-			    if (pub)
-				pbw.write("dist|" + x);
-			}
-		    }
-
-		    if (x != -1) {
-			pstr += " " + counter;
-
-			pstr += " " + adcstring;
-			pstr += " " + rearWheelSpeedData;
-			pstr += " " + frontWheelSpeedData;
-
-			pstr += " " + x;
-		    }
-
-		    //pstr = "";
-		    // light LED
-
-		    if (x > 2000) {
-			led.write("1|0");
-			led.write("2|1");
-			led.write("3|1");
-		    } else if (x == -1) {
-			led.write("1|0");
-			led.write("2|0");
-			led.write("3|0");
-		    } else if (x > 200) {
-			led.write("1|1");
-			led.write("2|0");
-			led.write("3|0");
-		    } else if (x > 150) {
-			led.write("1|1");
-			led.write("2|0");
-			led.write("3|1");
-		    } else if (x > 125) {
-			led.write("1|1");
-			led.write("2|1");
-			led.write("3|0");
-		    } else {
-			led.write("1|1");
-			led.write("2|1");
-			led.write("3|1");
-		    }
-
-		    //		    if (x < -1) {
-		    if (counter % 20 == 0) {
-			if (pub)
-			    pbw.write("all|" + pstr);
-
-			pstr = "";
-			mcnt = 0;
-
-			try {
-			    Thread.sleep(delay1);
-			} catch (InterruptedException e) {
-			    VM.println("Interrupted.\r\n");
-			}
-		    } else {
-			try {
-			    if (x != -1)
-				pstr += "; ";
-			    // 100 instead of 200 here -> failure to publish
-			    Thread.sleep(delay2);
-			} catch (InterruptedException e) {
-			    VM.println("Interrupted.\r\n");
-			}
-		    }
-
+		    Thread.sleep(w);
 		}
+	    }
+
+	    int x;
+	    if (true) {
+		// does it still hang if we don't receive what
+		// SCU sends?
+		// answer: yes
+
+		//Object obj = getval(ab);
+		Object obj = ab.receive();
+		if (obj != null) {
+		    String s = (String) obj;
+		    try {
+			x = Integer.parseInt(s);
+		    } catch (NumberFormatException e) {
+			VM.println("format exception (" + s + ")");
+			x = -1;
+		    }
+		} else {
+		    x = -1;
+		}
+		VM.println("counter = " + counter + ", dist = " + x);
+
+		if (false) {
+		    // does it still hang if we don't publish
+		    // what we received?
+		    // answer: yes
+		    Thread.sleep(w);
+
+		    if (pub)
+			pbw.write("dist|" + x);
+		}
+	    }
+
+	    if (x != -1 || true) {
+		pstr += " " + counter;
+
+		pstr += " " + adcstring;
+		pstr += " " + rearWheelSpeedData;
+		pstr += " " + frontWheelSpeedData;
+
+		pstr += " " + x;
+	    }
+
+	    //pstr = "";
+	    // light LED
+
+	    if (x > 2000) {
+		led.write("1|0");
+		led.write("2|1");
+		led.write("3|1");
+	    } else if (x == -1) {
+		led.write("1|0");
+		led.write("2|0");
+		led.write("3|0");
+	    } else if (x > 200) {
+		led.write("1|1");
+		led.write("2|0");
+		led.write("3|0");
+	    } else if (x > 150) {
+		led.write("1|1");
+		led.write("2|0");
+		led.write("3|1");
+	    } else if (x > 125) {
+		led.write("1|1");
+		led.write("2|1");
+		led.write("3|0");
+	    } else {
+		led.write("1|1");
+		led.write("2|1");
+		led.write("3|1");
+	    }
+
+	    //		    if (x < -1) {
+	    if (counter % 20 == 0) {
+		if (pub)
+		    pbw.write("all|" + pstr);
+
+		pstr = "";
+		mcnt = 0;
+
+		Thread.sleep(delay1);
+	    } else {
+		if (x != -1)
+		    pstr += "; ";
+		// 100 instead of 200 here -> failure to publish
+		Thread.sleep(delay2);
+	    }
+
 	}
+    }
 }
