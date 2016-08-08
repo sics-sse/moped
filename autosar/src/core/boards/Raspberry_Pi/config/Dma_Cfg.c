dcf0c7b831386dced106caeda4630a6b6ac96921
Arndt Jonasson <arndt@sics.se>
2016-08-08 11:23:21 +0200

    Spelling corrections.

--- autosar/src/core/boards/Raspberry_Pi/config/Dma_Cfg.c
+++ autosar/src/core/boards/Raspberry_Pi/config/Dma_Cfg.c
@@ -12,12 +12,12 @@ const Dma_ChannelConfigType DmaChannelConfig [DMA_NUMBER_OF_CHANNELS] =
 {
 	{
 		.DMA_CHANNEL_PRIORITY = DMA_CHANNEL0,
-		.DMA_CHANNEL_PREEMTION_ENABLE = BIT(1)		// Sets the value of the ENABLE register (enabling dma engine 0)
+		.DMA_CHANNEL_PREEMPTION_ENABLE = BIT(1)		// Sets the value of the ENABLE register (enabling dma engine 0)
 	},
 
 	{
 		.DMA_CHANNEL_PRIORITY = DMA_CHANNEL1,
-		.DMA_CHANNEL_PREEMTION_ENABLE = BIT(2)		// Sets the value of the ENABLE register (enabling dma engine 1)
+		.DMA_CHANNEL_PREEMPTION_ENABLE = BIT(2)		// Sets the value of the ENABLE register (enabling dma engine 1)
 	},
 };
 
