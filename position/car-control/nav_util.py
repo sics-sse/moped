6ddeab74bf505738f985c0564b06c258cb151306
Arndt Jonasson <arndt@sics.se>
2017-01-03 08:55:33 +0100

    Module for generally useful functions.

---
+++ position/car-control/nav_util.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+def dist(x1, y1, x2, y2):
+    return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+
+def sign(x):
+    if x < 0:
+        return -1
+    if x > 0:
+        return 1
+    return 0
+
