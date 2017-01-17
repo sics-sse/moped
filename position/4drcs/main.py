6924b251b2c68c66e95e34e9febde223c7f942e8
Arndt Jonasson <arndt@sics.se>
2017-01-17 08:50:54 +0100

    Put vehicle-level stuff in vehicle.py.

--- position/4drcs/main.py
+++ position/4drcs/main.py
@@ -1,32 +1,16 @@
-import signal
 import sys
 import os
 import time
-import threading
 
 import logging
 
-from node1 import Node1
-from node2 import Node2
+from vehicle import Vehicle
 
-import worldmodel
-
-wm = worldmodel.WM()
-
-n1 = Node1(wm)
-n2 = Node2(wm)
-
-n1.lower = n2
-n2.upper = n1
-
-threading.Thread(target=worldmodel.worldmodel, args=(wm,)).start()
-
-res = n1.task(('goto', 2))
-print("task returned %s" % str(res))
+v = Vehicle()
 
 logging.done()
 
-wm.alldone = True
+v.wm.alldone = True
 
 if False:
     try:
