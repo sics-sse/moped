c3894bba218d97b743478cfef7aeeebbc00fdd05
Arndt Jonasson <arndt@sics.se>
2017-01-18 08:54:53 +0100

    Two vehicles.

--- position/4drcs/main.py
+++ position/4drcs/main.py
@@ -6,11 +6,30 @@ import logging
 
 from vehicle import Vehicle
 
-v = Vehicle()
+v1 = Vehicle("1")
+
+v2 = Vehicle("2")
+v2.wm.y = 1.0
+
+if False:
+    # go the same route, v2 has a head start
+    v1.task(('goto', 2))
+#time.sleep(10)
+    v2.task(('goto', 2))
+    v2.wm.speedlimit = 0.4
+else:
+    # go different routes, cross at some point: v2 should give way to v1
+    v1.task(('goto', 1))
+#time.sleep(10)
+    v2.task(('goto', 2))
+
+v1.thread.join()
+v2.thread.join()
 
 logging.done()
 
-v.wm.alldone = True
+v1.wm.alldone = True
+v2.wm.alldone = True
 
 if False:
     try:
