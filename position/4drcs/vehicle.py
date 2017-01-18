6dad5908dc5b183a5344327ef561eaf323f9ae9a
Arndt Jonasson <arndt@sics.se>
2017-01-18 08:58:12 +0100

    Introduced vehicles. Don't start a task automatically when the vehicle is created.

--- position/4drcs/vehicle.py
+++ position/4drcs/vehicle.py
@@ -5,18 +5,30 @@ from node2 import Node2
 
 import worldmodel
 
+def taskthread(v, t):
+    res = v.n1.task(t)
+    print("task returned %s" % str(res))
+
 class Vehicle:
-    def __init__(self):
+    def __init__(self, name):
         self.wm = worldmodel.WM()
 
-        self.n1 = Node1(self.wm)
-        self.n2 = Node2(self.wm)
+        self.name = name
+
+        self.n1 = Node1(self)
+        self.n2 = Node2(self)
 
         self.n1.lower = self.n2
         self.n2.upper = self.n1
 
-        threading.Thread(target=worldmodel.worldmodel, args=(self.wm,)).start()
+        self.theclass = Vehicle
 
-        res = self.n1.task(('goto', 2))
-        print("task returned %s" % str(res))
+        Vehicle.vehicles.append(self)
 
+        threading.Thread(target=worldmodel.worldmodel, args=(self,)).start()
+
+    def task(self, t):
+        self.thread = threading.Thread(target=taskthread, args=(self, t))
+        self.thread.start()
+
+    vehicles = []
