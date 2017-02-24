3ee61a124ee9d584c55354632035678d99477f22
Arndt Jonasson <arndt@sics.se>
2017-02-24 09:12:02 +0100

    New output function with several levels. Use a following goal when it appears.

--- position/car-control2/nav1.py
+++ position/car-control2/nav1.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import random
 import queue
@@ -5,7 +6,7 @@ import queue
 import nav_log
 from nav_log import tolog, tolog0
 
-from nav_util import sign, dist, start_new_thread, rev
+from nav_util import sign, dist, start_new_thread, rev, min
 
 import eight
 import driving
@@ -15,7 +16,12 @@ import nav_tc
 def nav1init():
     g.user_pause = False
     g.randdict = dict()
+    g.lev = 1
 
+
+def out(lev, s):
+    if lev <= g.lev:
+        sys.stdout.write(s + "\n")
 
 def pause():
     g.user_pause = True
@@ -75,7 +81,7 @@ def whole4aux(path0):
     start_new_thread(executor1, (qtolower, qfromlower))
 
     nextplan = None
-    thengoal = None
+    thengoal0 = None
 
     while True:
         if nextplan == None:
@@ -83,32 +89,42 @@ def whole4aux(path0):
             qfromplanner.task_done()
         p = nextplan
         nextplan = None
-        thengoal = None
+        thengoal0 = None
         if p == 'stop':
-            print("executor0 got stop")
+            out(1, "executor0 got stop")
             driving.drive(0)
             return
         for status in executor0(p, qtolower, qfromlower):
             if status == 0:
-                print("executor0 failed, whole4aux exits")
+                out(2, "executor0 failed, whole4aux exits")
                 driving.drive(0)
                 return
             else:
-                print("executor0 reports %d empty=%s" % (
+                out(2, "executor0 reports %d empty=%s" % (
                         status, str(qfromplanner.empty())))
                 if not qfromplanner.empty():
                     if nextplan == None:
                         nextplan = qfromplanner.get()
                         qfromplanner.task_done()
-                        print("next plan for executor0 is %s" % (str(nextplan)))
-                        thengoal = nextplan[1]
+                        out(2, "next plan for executor0 is %s" % (str(nextplan)))
+                        if nextplan != 'stop':
+                            thengoal0 = nextplan[1]
                         # ugly hack, for testing
-                        thengoal0 = thengoal
                     else:
-                        print("another new plan for executor0 exists, but we already have one: %s" % str(nextplan))
+                        out(2, "another new plan for executor0 exists, but we already have one: %s" % str(nextplan))
 
 def planner0x(qfromplanner, qtoplanner):
-    qfromplanner.put([34,23,5,34,23])
+    if True:
+        qfromplanner.put([34, 35])
+        qfromplanner.put([35, 6])
+        qfromplanner.put([6, 23, 34])
+        qfromplanner.put([34, 5, 6])
+    else:
+        qfromplanner.put([34, 35])
+        qfromplanner.put([35, 6])
+        qfromplanner.put([6, 5, 34])
+        qfromplanner.put([34, 23, 6])
+
     qfromplanner.put('stop')
 
 def planner0(qfromplanner, qtoplanner):
@@ -186,24 +202,33 @@ def planner0(qfromplanner, qtoplanner):
         
 def executor0(path, qtolower, qfromlower):
 
+    out(1, "executor0 got plan %s" % str(path))
+
     for i in range(0, len(path)-1):
         path1 = [path[i], path[i+1]]
         if i < len(path)-2:
             path1.append(path[i+2])
 
+        # path1 has either 2 or 3 elements
         qtolower.put(path1)
         while True:
-            print("executor0's thengoal = %s" % str(thengoal0))
+            out(2, "executor0's thengoal = %s" % str(thengoal0))
             status = qfromlower.get()
             qfromlower.task_done()
             if status == 0:
-                print("executor1 failed; aborting")
+                out(2, "executor1 failed; aborting")
                 yield status
             elif status == 1:
-                print("executor1 reported %d" % status)
+                out(2, "executor1 reported %d" % status)
                 break
             else:
-                print("executor1 reported %d" % (status))
+                out(2, "executor1 reported %d" % (status))
+                if len(path1) and thengoal0 != None:
+                    out(2, "executor0 extending plan")
+                    qtolower.put(('extend', thengoal0))
+                    path1.append(thengoal0)
+                else:
+                    qtolower.put('pass')
                 yield 2
 
         yield 1
@@ -221,96 +246,147 @@ def executor1(qfromhigher, qtohigher):
 
         path1 = qfromhigher.get()
         qfromhigher.task_done()
-        print("executor1: got task %s" % (str(path1)))
+        out(1, "executor1: got task %s" % (str(path1)))
+        thengoal = None
 
         qtoplanner.put(('path', path1))
         (p, plen) = qfromplanner.get()
         qfromplanner.task_done()
-        print("executor1: got plan %s" % (str(p)))
+        contflag = False
         while True:
+            px = [i for (i, _) in p]
+
+            out(1, "executor1: got plan %d %s len %d" % (p[0][1], str(px), plen))
             for status in gopath(p, plen):
                 if status == 0:
-                    print("gopath failed; aborting")
+                    out(2, "gopath failed; aborting")
                     qtohigher.put(status)
                     # I think we should go the top of the outer loop,
                     # but returning at least aborts for real
                     return
                 else:
-                    # print("gopath reports %d" % status)
+                    # out(2, "gopath reports %d" % status)
                     # we should let the planner create this plan in advance,
                     # shouldn't we?
-                    qtoplanner.put(('next',))
+                    qtoplanner.put(('next', thengoal))
                     (p, plen) = qfromplanner.get()
                     qfromplanner.task_done()
-                    print("executor1: suggested new plan %s len %d" % (
+                    out(2, "executor1: suggested new plan %s len %d" % (
                             str(p), plen))
                     qtohigher.put(2)
-                    continue
-            break
+                    ack = qfromhigher.get()
+                    qfromhigher.task_done()
+                    #out(2, "higher acked %s" % str(ack))
+                    if ack == 'pass':
+                        pass
+                    else:
+                        (_, thengoal) = ack
+
+                    if len(p) < 2:
+                        continue
+                    else:
+                        contflag = True
+                        break
+
+            if not contflag:
+                break
+            contflag = False
         # we will just have put 2, too
         qtohigher.put(1)
 
 def planner1(qfromplanner, qtoplanner):
     plann = 0
+    path1_0 = None
     while True:
         info = qtoplanner.get()
         qtoplanner.task_done()
         if info[0] == 'path':
             plann += 1
-            path1_0 = info[1]
+            path1_0 = info[1][:]
             path1 = path1_0[0:2]
+            reachedmiddle = False
+            thengoal = None
             path1_e = eight.insert_waypoints_l(path1)
 
             pathlen = len(path1_e)
+            curpathlen = pathlen
 
             path2_e = eight.insert_waypoints_l(path1_0)
 
             path2_e = [(i, plann) for i in path2_e]
             path1_e = [(i, plann) for i in path1_e]
 
-            print("%s -> %s" % (str(path1), str(path1_e)))
-            print("%s -> %s" % (str(path1_0), str(path2_e)))
+            out(2, "path1 %s -> %s" % (str(path1), str(path1_e)))
+            out(2, "path2 %s -> %s" % (str(path1_0), str(path2_e)))
 
 
-            print("planner1: task %s then %s" % (str(path1), str(path1_0[2:])))
-            print(" -> plan %s" % str(path1_e))
+            out(2, "planner1: task %s then %s" % (str(path1), str(path1_0[2:])))
+            out(2, " -> plan %s" % str(path1_e))
         elif info[0] == 'next':
             # now we only pick the next point in our original plan,
             # but we should make a new plan each time.
             plann += 1
+
+            item = info[1]
+            out(2, "planner1: thengoal = %s" % str(item))
+            if thengoal == None and item != None:
+                if len(path1_0) == 2:
+                    # since we use the last element in path2_e below,
+                    # don't override an already existing nextpoint
+                    path2_e.append((item, plann))
+                    out(2, "path2 now %s" % (str(path2_e)))
+                    thengoal = item
+
+
             # pretend we did:
             path2_e = [(i, plann) for (i, _) in path2_e]
 
             if len(path2_e) > 1:
-                npath = [path2_e[1][0], path1_e[-1][0]]
-                print("path3_e %s -> " % (npath))
-                path3 = eight.insert_waypoints_l(npath)
-                path3_e = [(i, plann) for i in path3]
-                print(" -> %s" % (path3_e))
+                # we want npath to also include the intermediate
+                # decision points, if we haven't passed it
+                # no, we are not supposed to pass it
+                if path2_e[1][0] == path1_e[-1][0]:
+                    reachedmiddle = True
+                if reachedmiddle:
+                    npath = [path2_e[1][0], path2_e[-1][0]]
+                    path3_e = []
+                else:
+                    npath = [path2_e[1][0], path1_e[-1][0], path2_e[-1][0]]
+                    out(2, "path3_e %s -> " % (npath))
+                    path3 = eight.insert_waypoints_l(npath)
+                    path3_e = [(i, plann) for i in path3]
+                    out(2, " -> %s" % (path3_e))
                 path2_e = path3_e
             else:
+                # since len(path2_e) <= 1, this can be simplified:
                 path2_e = path2_e[1:]
                 path2_ex = path2_e
                 if len(path2_ex) >= pathlen:
                     path2_ex = path2_e[0:pathlen]
-                print(" -> updated plan %s" % str(path2_ex))
+                out(2, " -> updated plan %s" % str(path2_ex))
                 path2_e = path2_ex
+            curpathlen -= 1
+
         else:
-            print("planner1 got unexpected command: %s" % (str(info)))
+            out(2, "planner1 got unexpected command: %s" % (str(info)))
         # our caller doesn't know how long the part of the plan is which
         # corresponds to the actual goal, so we tell it how long
         # that part was originally
-        qfromplanner.put((path2_e, len(path1_e)))
+
+        qfromplanner.put((path2_e, curpathlen))
 
 
 def gopath(path00, plen):
     g.last_send = None
 
-    print("path00 = %s" % (str(path00)))
-
     path0 = [i for (i, _) in path00]
 
-    #print("speedsign = %d" % g.speedsign)
+    #out(2, "path00 = %s" % (str(path00)))
+    out(1, "gopath: %d %s" % (path00[0][1], str(path0[0:plen])))
+    if path00[plen:] != []:
+        out(1, "gopath: extra = %s" % (str(path0[plen:])))
+
+    #out(2, "speedsign = %d" % g.speedsign)
     g.speedsign = 1
 
     path = eight.piece2path(path0, -0.25)
@@ -328,6 +404,7 @@ def gopath(path00, plen):
 #    for j in range(0, len(path)):
     for j in range(0, plen):
         (i, x, y) = path[j]
+        out(1, "goto %s = %s" % (str(i), str(path00[j])))
         if g.remote_control:
             print("remote control/crash")
             yield 0
@@ -363,7 +440,7 @@ def gopath(path00, plen):
             status = 0
 
         if status != 0:
-            print("goto_1 returned %d for node %d; we are at (%f, %f)" % (
+            out(2, "goto_1 returned %d for node %d; we are at (%f, %f)" % (
                     status, i, g.ppx, g.ppy))
         if status == 2:
             #driving.drive(-20)
