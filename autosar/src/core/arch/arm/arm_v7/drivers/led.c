06ab64581533ff23d21d5d26f8d2f64987a82fc9
Arndt Jonasson <arndt@sics.se>
2017-05-31 14:02:43 +0200

    More correct LED initialization, but this code isn't used anyway.

--- autosar/src/core/arch/arm/arm_v7/drivers/led.c
+++ autosar/src/core/arch/arm/arm_v7/drivers/led.c
@@ -83,7 +83,7 @@ void led_asm(uint32 n)
    __asm
 		(
 				"Led_on:     				\
-				        ldr r0,=0x3F200000;	\
+				        ldr r0,=0x20200000;	\
 				        mov r1,#1;			\
 						lsl r1,#18;			\
 						str r1,[r0,#4];		\
