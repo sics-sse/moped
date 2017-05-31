92beb1dac68aea9d035b511dd06835286f12dea8
Arndt Jonasson <arndt@sics.se>
2017-05-31 13:51:47 +0200

    Experiment with a second sensor on GPIO 17/18.

--- autosar/src/core/arch/arm/arm_v6/drivers/sensors/UltrasonicSensor.c
+++ autosar/src/core/arch/arm/arm_v6/drivers/sensors/UltrasonicSensor.c
@@ -18,6 +18,9 @@
 #include "bcm2835.h"
 #include "Sensors.h"
 
+#define GPIO_ULTRA_TRIG2 17
+#define GPIO_ULTRA_ECHO2 18
+
 /* Records whether this module has already been initialized */
 static boolean ultra_init = false;
 
@@ -30,6 +33,12 @@ void UltrasonicSensor_Init(void){
     bcm2835_GpioFnSel(GPIO_ULTRA_ECHO, GPFN_IN);   				// input pin, for listening to echo signals from the sensor
 
     bcm2835_SetReadWriteGpioReg(&GPCLR0, GPIO_ULTRA_TRIG);          // Don't trigger just yet (set trigger output to low)
+
+	/* Select functionality for the pins connected to the ultrasonic sensor */
+	bcm2835_GpioFnSel(GPIO_ULTRA_TRIG2, GPFN_OUT);  				// output pin, for triggering the sensor
+    bcm2835_GpioFnSel(GPIO_ULTRA_ECHO2, GPFN_IN);   				// input pin, for listening to echo signals from the sensor
+
+    bcm2835_SetReadWriteGpioReg(&GPCLR0, GPIO_ULTRA_TRIG2);          // Don't trigger just yet (set trigger output to low)
 
     bcm2835_Sleep(300);
     ultra_init = true;
@@ -46,7 +55,7 @@ void UltrasonicSensor_Init(void){
  */
 uint32 UltrasonicSensor_Read(void) {
 	uint64 startTime;
-	uint32 travelTime, distance;
+	uint32 travelTime, travelTime2, distance;
 
 	/* Initialize, if not already done */
 	if (ultra_init == false){
@@ -60,13 +69,16 @@ uint32 UltrasonicSensor_Read(void) {
 
 	/* Wait for the echo start and record its time */
 	int count1 = 0;
+	int lostpulse = 0;
 	/* Very occasionally, it doesn't become HIGH, maybe because we
 	   were interrupted and missed it. Make sure we don't get stuck in
 	   here. */
 	while (bcm2835_ReadGpioPin(&GPLEV0, GPIO_ULTRA_ECHO) == LOW) {
 	  count1++;
-	  if (count1 > 10000)
-	    return 0;
+	  if (count1 > 10000) {
+	    lostpulse = 1;
+	    break;
+	  }
 	}
 	startTime = CURRENT_TIME;
 
@@ -84,9 +96,61 @@ uint32 UltrasonicSensor_Read(void) {
 	  travelTime = 200000;
 	else
 	  travelTime = (uint32)(CURRENT_TIME - startTime);
-	//printf("ultra count = %d time = %d\r\n", count, travelTime);
+	printf("ultra count = %d time = %d %d\r\n",
+	       count, travelTime, lostpulse);
+
+
+
+#if 0
+
+	/* Send out a trigger signal and let it sound for around 10 microseconds */
+	bcm2835_SetGpioPin(GPIO_ULTRA_TRIG2);
+	bcm2835_Sleep(10);
+	bcm2835_ClearGpioPin(GPIO_ULTRA_TRIG2);
+
+	/* Wait for the echo start and record its time */
+	int count2_1 = 0;
+	int lostpulse2 = 0;
+	/* Very occasionally, it doesn't become HIGH, maybe because we
+	   were interrupted and missed it. Make sure we don't get stuck in
+	   here. */
+	while (bcm2835_ReadGpioPin(&GPLEV0, GPIO_ULTRA_ECHO2) == LOW) {
+	  count2_1++;
+	  if (count2_1 > 10000) {
+	    lostpulse2 = 1;
+	    break;
+	  }
+	}
+	startTime = CURRENT_TIME;
+
+	/* Wait for the echo end and record the time of the echo period */
+	int count2 = 0;
+	while (bcm2835_ReadGpioPin(&GPLEV0, GPIO_ULTRA_ECHO2) == HIGH) {
+	  count2++;
+	  // about 3000 us/m, so the maximum range 4m = 12000 us, so let's
+	  // break when > 20000 us.
+	  // One loop here takes about 1 us.
+	  if (count2 > 20000)
+	    break;
+	}
+	if (count2 > 20000)
+	  travelTime2 = 200000;
+	else
+	  travelTime2 = (uint32)(CURRENT_TIME - startTime);
+	printf("ultra count2 = %d time = %d %d\r\n",
+	       count2, travelTime2, lostpulse2);
+
+
+
+
+
+
 
 	/* Calc the distance in cm (according to sensors datasheet) and return */
+	distance = travelTime2 * 0.017;
+
+#else
 	distance = travelTime * 0.017;
+#endif
 	return distance;
 }
