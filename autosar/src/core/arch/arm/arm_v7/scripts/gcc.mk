fe1d96a44df02371520d93067ae79223a3dbe993
Arndt Jonasson <arndt@sics.se>
2017-05-31 13:54:05 +0200

    Correct cpu architecture.

--- autosar/src/core/arch/arm/arm_v7/scripts/gcc.mk
+++ autosar/src/core/arch/arm/arm_v7/scripts/gcc.mk
@@ -1,13 +1,11 @@
 # prefered version
 CC_VERSION=4.4.5
-# ARMv6, Thumb, little endian, soft-float. 
-#cflags-y 	+= -O2  -mcpu=cortex-a7 -mfpu=neon-vfpv4
-cflags-y 	+= -O2  -mcpu=arm1176jz-s -mfpu=neon-vfpv4
+# ARMv7
+cflags-y 	+= -O2  -mcpu=cortex-a7 -mfpu=neon-vfpv4
 cflags-y 	+= -ggdb
 
 cflags-y += -ffunction-sections
 
 lib-y   	+= -lgcc -lc
-#ASFLAGS 	+= -mcpu=cortex-a7
-ASFLAGS 	+= -mcpu=arm1176jz-s
+ASFLAGS 	+= -mcpu=cortex-a7
 
