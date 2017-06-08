81aa5ceda807a14592df355b26d500805006e529
Arndt Jonasson <arndt@sics.se>
2017-06-08 09:44:27 +0200

    Obsolete, removed.

--- test/src/test/SoapTestPublisher.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import javax.xml.ws.Endpoint;
-
-public class SoapTestPublisher {
-	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:9999/moped/test", new SoapTest());
-	}
-}
