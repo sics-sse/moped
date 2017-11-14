4349c346b0457ce2e346c472af06f245d811f278
Arndt Jonasson <arndt@sics.se>
2017-11-14 11:01:39 +0100

    Don't crash as soon as numbers are in the wrong format.

--- ecm-core/src/main/java/network/external/CarDriver.java
+++ ecm-core/src/main/java/network/external/CarDriver.java
@@ -85,13 +85,19 @@ public class CarDriver implements Runnable {
 		 * @param message		----- a Wirelessino speed&steer command, such as "H0050V-097\0"
 		 * @return				----- speed&steer command in VCU-readable format, such as [50, -97]
 		 */
+
 		private byte[] interpretWirelessino(String message) {
 			byte[] res = new byte[2];
 			
-			System.out.println("RC message " + message);
+			//System.out.println("RC message " + message);
 
-			res[0] = Byte.parseByte(message.substring(1, 5));
-			res[1] = Byte.parseByte(message.substring(6, 10));
+			try {
+			    res[0] = Byte.parseByte(message.substring(1, 5));
+			    res[1] = Byte.parseByte(message.substring(6, 10));
+			} catch (NumberFormatException e) {
+			    res[0] = (byte) 'S';
+			    res[1] = (byte) '7';
+			}
 				
 			return res;
 		}
@@ -104,8 +110,13 @@ public class CarDriver implements Runnable {
 			/* We only want to read the last 11 bytes from the input stream (5 for each bar + EOF) */
 			byte[] incomingBytes = new byte[11];
 			
+			long unixTime2 = 0;
+			long unixTime3 = 0;
+
 			try {
 				while (true) {
+
+				    long unixTime = System.currentTimeMillis();
 					int nrIncomingBytes = in.available();
 					if (nrIncomingBytes > 0) {
 						/* Skip all bytes except those that represent the last speed&steer command */
@@ -113,17 +124,32 @@ public class CarDriver implements Runnable {
 						in.skip(nrIncomingBytes-incomingBytes.length);
 						in.read(incomingBytes);
 						
+						long dff = unixTime - unixTime3;
+						if (dff > 600) {
+						    System.out.println(unixTime + " too long " + dff);
+						}
+
+						unixTime2 = unixTime;
+						unixTime3 = unixTime;
+
 						/* Convert the data into a format that will be understood on the recipient side and send it */ 
 						String str = new String(incomingBytes, "UTF-8");
 						byte[] data = interpretWirelessino(str);
 						PWMMessage pwmMessage;
 						if (str.charAt(0) == 'S') {
 							pwmMessage = new PWMMessage(1, incomingBytes);
+							//ecm.process(pwmMessage);
 						} else
 						    {
 							pwmMessage = new PWMMessage(pwmEcuId, data);
+							ecm.process(pwmMessage);
 						    }
-						ecm.process(pwmMessage);
+					} else {
+					    long dff = unixTime - unixTime2;
+					    if (dff > 600) {
+						System.out.println(unixTime + " too long0 " + dff);
+						unixTime2 = unixTime;
+					    }
 					}
 
 					Thread.sleep(10);
